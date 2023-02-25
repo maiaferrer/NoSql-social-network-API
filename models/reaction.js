3ef6fcd6524b9e3461921528e96c8b28921ae89d
maiaferrer <mferrer2@uncc.edu>
2023-02-24 21:26:56 -0500

    added schema to thoughts and reactions

---
+++ models/reaction.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Schema.Types.ObjectId()
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (date) => {
+        // Format date as MM/DD/YYYY
+        return date.toLocaleDateString();
+      },
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280,
+    }
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
---
+++ models/thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./reaction');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (date) => {
+    // Format date as MM/DD/YYYY
+    return date.toLocaleDateString();
+        },
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [
+        reactionSchema
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
